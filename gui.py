d5d6ca951741b4612145ada9edc7d8a971d3a1a4
Edwin Laboy <elaboytorres@gmail.com>
2021-12-21 17:53:08 -0400

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,87 @@
+from tkinter import * 
+from subprocess import * 
+from tkinter.filedialog import askopenfilename
+
+#for multiple files import askopenfiles!
+
+window = Tk()
+window.geometry('800x400')
+
+#Labels and Grids column Zero 
+receptor_label = Label(window, text = "Choose the receptor in pdbqt format")
+receptor_label.grid(column=0, row=0, sticky=W)
+center_x_label = Label(window, text="Center X")
+center_x_label.grid(column=0, row=1, sticky=W)
+center_y_label = Label(window, text="Center Y")
+center_y_label.grid(column=0, row=2, sticky=W)
+center_z_label = Label(window, text="Center Z")
+center_z_label.grid(column=0, row=3, sticky=W)
+size_x_label = Label(window, text="Size X")
+size_x_label.grid(column=0, row=4, sticky=W)
+size_y_label = Label(window, text="Size Y")
+size_y_label.grid(column=0, row=5, sticky=W)
+size_z_label = Label(window, text="Size Z")
+size_z_label.grid(column=0, row=6, sticky=W)
+ligand_label = Label(window, text = "Choose the ligand in pdbqt format")
+ligand_label.grid(column=0, row=7, sticky=W)
+exhaustiveness_label = Label(window, text="Exhaustiveness")
+exhaustiveness_label.grid(column=0, row=8, sticky=W)
+out_label = Label(window, text="Choose a name for the output file. For example: example_out.pdbqt")
+out_label.grid(column=0, row=9, sticky=W)
+
+# Column 1
+def entry():
+    return Entry(window, width=50)
+
+receptor = entry()
+receptor.grid(column=1, row=0)
+center_x = entry()
+center_x.grid(column=1, row=1)
+center_y = entry()
+center_y.grid(column=1, row=2)
+center_z = entry()
+center_z.grid(column=1, row=3)
+size_x = entry()
+size_x.grid(column=1, row=4)
+size_y = entry()
+size_y.grid(column=1, row=5)
+size_z = entry()
+size_z.grid(column=1, row=6)
+ligand = entry()
+ligand.grid(column=1, row=7)
+exhaustiveness = entry()
+exhaustiveness.grid(column=1, row=8)
+out = entry()
+out.grid(column=1, row=9)
+
+#Column 3 (buttons)
+def browse(txt_box):
+    global status
+    status = 0
+    filename = askopenfilename()
+    status = 1
+    txt_box.insert(END, filename)
+    return [filename, status]
+
+
+receptor_btn = Button(window, text="Browse", command=lambda: browse(receptor))
+receptor_btn.grid(column=2, row=0)
+if browse == 1: 
+    browse(receptor)
+
+ligand_btn = Button(window, text="Browse", command=lambda: browse(ligand))
+ligand_btn.grid(column=2, row=7)
+if browse == 1: 
+    browse(ligand)
+
+
+
+
+
+
+#def config_file():
+
+
+
+
+window.mainloop()
